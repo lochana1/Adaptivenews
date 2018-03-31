317a8833d5e121726fd5ed4f4e80714bc5879116
Zen <zhengxu5511@gmail.com>
2018-03-31 23:18:02 +0100

    show chars

--- src/pages/show-my-interest/show-my-interest.ts
+++ src/pages/show-my-interest/show-my-interest.ts
@@ -1,7 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Chart } from 'chart.js';
-
+import {UserData} from "../../providers/user-data";
+import {NewsanduserProvider} from "../../providers/newsanduser/newsanduser";
 /**
  * Generated class for the ShowMyInterestPage page.
  *
@@ -22,38 +23,51 @@ export class ShowMyInterestPage {
     barChart: any;
     doughnutChart: any;
     lineChart: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    responseData:any;
+    userName:any;
+    password:any;
+    weights:any;
+    type:any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public userData : UserData, private news: NewsanduserProvider) {
   }
 
   ionViewDidLoad() {
-           this.barChart = new Chart(this.barCanvas.nativeElement, {
- 
+
+     this.userData.getUsername().then((id)=> {
+      this.userName = id;
+      this.userData.getpwd().then((id)=> {
+        this.password = id;
+        this.type = "/learning/userweights/"+this.userName +"/"+this.password;
+        console.log(this.type)
+        this.news.getData(this.type).then((value: any[]) => {
+          this.weights = value[0];
+          console.log(this.weights);
+          this.barChart = new Chart(this.barCanvas.nativeElement, {
             type: 'bar',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: ["Sports", "Business", "Entertainment", "Politics", "Technology"],
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    label: '# of Interest',
+                    data: [this.weights.sports, this.weights.business, this.weights.entertainment, this.weights.politics, this.weights.technology],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
                         'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(153, 102, 255, 0.2)'
                     ],
                     borderColor: [
                         'rgba(255,99,132,1)',
                         'rgba(54, 162, 235, 1)',
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        'rgba(153, 102, 255, 1)'
                     ],
                     borderWidth: 1
                 }]
             },
             options: {
+                responsive: true,
                 scales: {
                     yAxes: [{
                         ticks: {
@@ -62,71 +76,14 @@ export class ShowMyInterestPage {
                     }]
                 }
             }
- 
+          });
+        })
+        .catch((err) => {
+          console.log("Error in getting weights data:")
+          console.log(err);
         });
- 
-        this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
- 
-            type: 'doughnut',
-            data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    hoverBackgroundColor: [
-                        "#FF6384",
-                        "#36A2EB",
-                        "#FFCE56",
-                        "#FF6384",
-                        "#36A2EB",
-                        "#FFCE56"
-                    ]
-                }]
-            }
- 
-        });
- 
-        this.lineChart = new Chart(this.lineCanvas.nativeElement, {
- 
-            type: 'line',
-            data: {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
-                datasets: [
-                    {
-                        label: "My First dataset",
-                        fill: false,
-                        lineTension: 0.1,
-                        backgroundColor: "rgba(75,192,192,0.4)",
-                        borderColor: "rgba(75,192,192,1)",
-                        borderCapStyle: 'butt',
-                        borderDash: [],
-                        borderDashOffset: 0.0,
-                        borderJoinStyle: 'miter',
-                        pointBorderColor: "rgba(75,192,192,1)",
-                        pointBackgroundColor: "#fff",
-                        pointBorderWidth: 1,
-                        pointHoverRadius: 5,
-                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                        pointHoverBorderColor: "rgba(220,220,220,1)",
-                        pointHoverBorderWidth: 2,
-                        pointRadius: 1,
-                        pointHitRadius: 10,
-                        data: [65, 59, 80, 81, 56, 55, 40],
-                        spanGaps: false,
-                    }
-                ]
-            }
- 
-        });
- 
+      });
+    });
     }
  
  
